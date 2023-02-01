683ab9c4bec70f9791f995c1e7a306a201a4b47b
rabin245 <drarabin@gmail.com>
2023-02-02 00:19:30 +0545

    new responsive sidebar design

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,37 +1,122 @@
-function AdminDashboard() {
-  return (
-    <div className="adminDashboard relative">
-      <div className="p-3 bg-blue-500 flex justify-between items-center">
-        <span className="font-semibold text-white text-3xl">
-          Admin dashboard
-        </span>
+import logo from "/vite.svg";
+import { useState } from "react";
 
-        <div className="w-8 h-8 flex justify-center items-center bg-teal-700 rounded-full text-white font-medium text-base cursor-pointer  active:ring-white active:ring-1">
-          R
+function AdminDashboard() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="mainDiv">
+      {/* toggle button */}
+      <div
+        className={`toggleButton absolute top-3 left-3 z-1 p-2 text-white cursor-pointer bg-gray-800 rounded-lg shadow-xl ${
+          open ? "hidden" : "block"
+        }`}
+        onClick={() => setOpen(!open)}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            fillRule="evenodd"
+            d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75H12a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </div>
+      {/* sidebar */}
+      <div
+        className={`sidebar fixed top-0 bottom-0 bg-gray-900 p-2 lg:left-0 overflow-y-auto w-64 text-center z-2 shadow-xl shadow-black
+         ${open ? "left-0 " : "-left-64"} 
+          transition-all duration-500 ease-in-out
+
+        `}
+      >
+        <div className="headingPart text-gray-100 mt-1 flex items-center justify-between">
+          <div className="flex items-center p-2">
+            <img src={logo} alt="logo" className="h-8 " />
+            <h1 className="text-2xl font-bold ml-3">Election</h1>
+          </div>
+          {/* close button */}
+          <span
+            className="mr-1 cursor-pointer lg:hidden"
+            onClick={() => setOpen(!open)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-5 "
+            >
+              <path
+                fillRule="evenodd"
+                d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </span>
+        </div>
+
+        <hr className="bg-gray-600 h-px border-0 my-2" />
+
+        <div className="p-2 flex items-center text-white cursor-pointer px-4 hover:bg-blue-600 rounded-md mt-3">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-6"
+          >
+            <path
+              fillRule="evenodd"
+              d="M9 1.5H5.625c-1.036 0-1.875.84-1.875 1.875v17.25c0 1.035.84 1.875 1.875 1.875h12.75c1.035 0 1.875-.84 1.875-1.875V12.75A3.75 3.75 0 0016.5 9h-1.875a1.875 1.875 0 01-1.875-1.875V5.25A3.75 3.75 0 009 1.5zm6.61 10.936a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 14.47a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+              clipRule="evenodd"
+            />
+            <path d="M12.971 1.816A5.23 5.23 0 0114.25 5.25v1.875c0 .207.168.375.375.375H16.5a5.23 5.23 0 013.434 1.279 9.768 9.768 0 00-6.963-6.963z" />
+          </svg>
+          <span className="font-bold text-gray-200 text-xl ml-4">Home</span>
+        </div>
+        <div className="p-2 flex items-center text-white cursor-pointer px-4 hover:bg-blue-600 rounded-md mt-3">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              fillRule="evenodd"
+              d="M1.5 5.625c0-1.036.84-1.875 1.875-1.875h17.25c1.035 0 1.875.84 1.875 1.875v12.75c0 1.035-.84 1.875-1.875 1.875H3.375A1.875 1.875 0 011.5 18.375V5.625zM21 9.375A.375.375 0 0020.625 9h-7.5a.375.375 0 00-.375.375v1.5c0 .207.168.375.375.375h7.5a.375.375 0 00.375-.375v-1.5zm0 3.75a.375.375 0 00-.375-.375h-7.5a.375.375 0 00-.375.375v1.5c0 .207.168.375.375.375h7.5a.375.375 0 00.375-.375v-1.5zm0 3.75a.375.375 0 00-.375-.375h-7.5a.375.375 0 00-.375.375v1.5c0 .207.168.375.375.375h7.5a.375.375 0 00.375-.375v-1.5zM10.875 18.75a.375.375 0 00.375-.375v-1.5a.375.375 0 00-.375-.375h-7.5a.375.375 0 00-.375.375v1.5c0 .207.168.375.375.375h7.5zM3.375 15h7.5a.375.375 0 00.375-.375v-1.5a.375.375 0 00-.375-.375h-7.5a.375.375 0 00-.375.375v1.5c0 .207.168.375.375.375zm0-3.75h7.5a.375.375 0 00.375-.375v-1.5A.375.375 0 0010.875 9h-7.5A.375.375 0 003 9.375v1.5c0 .207.168.375.375.375z"
+              clipRule="evenodd"
+            />
+          </svg>
+
+          <span className="font-bold text-gray-200 text-xl ml-4">Results</span>
+        </div>
+
+        <hr className="bg-gray-600 h-px border-0 my-2" />
+
+        <div className="p-2 flex items-center text-white cursor-pointer px-4 hover:bg-blue-600 rounded-md mt-3">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              fillRule="evenodd"
+              d="M7.5 3.75A1.5 1.5 0 006 5.25v13.5a1.5 1.5 0 001.5 1.5h6a1.5 1.5 0 001.5-1.5V15a.75.75 0 011.5 0v3.75a3 3 0 01-3 3h-6a3 3 0 01-3-3V5.25a3 3 0 013-3h6a3 3 0 013 3V9A.75.75 0 0115 9V5.25a1.5 1.5 0 00-1.5-1.5h-6zm5.03 4.72a.75.75 0 010 1.06l-1.72 1.72h10.94a.75.75 0 010 1.5H10.81l1.72 1.72a.75.75 0 11-1.06 1.06l-3-3a.75.75 0 010-1.06l3-3a.75.75 0 011.06 0z"
+              clipRule="evenodd"
+            />
+          </svg>
+
+          <span className="font-bold text-gray-200 text-xl ml-4">Logout</span>
         </div>
       </div>
 
-      <div className="flex">
-        <div className="w-64 h-screen bg-blue-300 sticky top-0 flex justify-center ">
-          <ul className="pt-12 w-full flex flex-col text-white font-bold  ">
-            <li className="hover:bg-blue-500 hover:shadow w-full text-center cursor-pointer">
-              Dashboard
-            </li>
-            <li className="hover:bg-blue-500 hover:shadow  w-full text-center cursor-pointer  ">
-              Results
-            </li>
-          </ul>
-        </div>
-
-        <main className="p-2 flex flex-col justify-center items-center w-full">
-          <button className="bg-blue-400 rounded-2xl p-5 text-white text-2xl font-bold hover:shadow-xl active:ring active:ring-blue-100">
-            Start election
-          </button>
-          <button className="mt-8 bg-blue-400 rounded-2xl p-5 text-white text-2xl font-bold hover:shadow-xl active:ring active:ring-blue-100">
-            End election
-          </button>
-          <span className="mt-8">The election has not started yet.</span>
-        </main>
+      {/* main body */}
+      <div className="mainContent h-full p-4 bg-white lg:ml-64">
+        <h1 className="text-6xl text-gray-700 font-bold">hello world</h1>
       </div>
     </div>
   );
