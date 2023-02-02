bfeca05415a5fffc8962de08df4c8b5c229231dc
pras557 <prasmitneupane1@gmail.com>
2023-02-02 08:19:48 +0545

    added the main body design

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -115,8 +115,21 @@ function AdminDashboard() {
       </div>
 
       {/* main body */}
-      <div className="mainContent h-full p-4 bg-white lg:ml-64">
-        <h1 className="text-6xl text-gray-700 font-bold">hello world</h1>
+      <div className="mainContent h-screen p-4 bg-white lg:ml-64 flex flex-col justify-center items-center">
+        <button className="bg-blue-500 p-4 w-44 text-2xl font-bold text-white rounded-lg hover:shadow-md hover:bg-blue-600 active:ring-1 active:ring-gray-400">
+          Start
+        </button>
+
+        <button className="mt-4 bg-blue-500 p-4 w-44 text-2xl font-bold text-white rounded-lg hover:shadow-md hover:bg-blue-600 active:ring-1 active:ring-gray-400">
+          End
+        </button>
+
+        <p className="mt-4 w-3/4">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae autem
+          vitae sed aspernatur illum ipsa corrupti nemo animi nobis, id
+          assumenda, accusantium voluptatem corporis cumque sint eum est ducimus
+          ut!
+        </p>
       </div>
     </div>
   );
