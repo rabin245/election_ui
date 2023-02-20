2997175a044a5e8780a1431943476c877478a390
rabin245 <drarabin@gmail.com>
2023-02-20 14:56:34 +0545

    added routes with react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
 import AdminDashboard from "./pages/AdminDashboard";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import SideBar from "./components/SideBar";
+
+const Layout = () => {
+  return (
+    <>
+      <SideBar />
+      <Outlet />
+    </>
+  );
+};
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <AdminDashboard />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <AdminDashboard />
+      <RouterProvider router={router} />
     </div>
   );
 }
