f68bd49203a959e1007fb7cb0ab350c5f9cd6d4d
rabin245 <drarabin@gmail.com>
2023-03-05 18:00:51 +0545

    mapping prop to static objects

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,22 @@
 import { Link } from "react-router-dom";
 
-const Card = ({ title, desc, icon, buttonText, color, link }) => {
+const Card = ({ title, desc, icon, color, buttonText, link }) => {
+  const colorVariants = {
+    blue: "border-blue-400 hover:bg-blue-100",
+    indigo: "border-indigo-400 hover:bg-indigo-100",
+    purple: "border-purple-400 hover:bg-purple-100",
+  };
+
+  const buttonColorVariants = {
+    blue: "border-blue-500 hover:bg-blue-500",
+    indigo: "border-indigo-500 hover:bg-indigo-500",
+    purple: "border-purple-500 hover:bg-purple-500",
+  };
+
   return (
     <div
-      className={`card rounded-lg border-4 border-${color}-400 bg-gray-100 hover:bg-${color}-100 
-                          flex flex-col items-center justify-between 
-                          p-4 min-h-[220px]`}
+      className={`card rounded-lg border-4 ${colorVariants[color]} bg-gray-100 
+                flex flex-col items-center justify-between p-4 min-h-[220px]`}
     >
       <div className="flex flex-col items-center">
         <div className="card-header text-xl font-semibold">
@@ -20,8 +31,8 @@ const Card = ({ title, desc, icon, buttonText, color, link }) => {
       </div>
       <Link to={`${link}`}>
         <div
-          className={`card-button border-2 border-${color}-500 rounded-full py-2 px-4 font-bold
-                          hover:bg-${color}-500 hover:text-white active:shadow-lg
+          className={`card-button border-2 ${buttonColorVariants[color]} rounded-full py-2 px-4 font-bold
+                           hover:text-white active:shadow-lg
                           transition-all duration-200 ease-in-out`}
         >
           {buttonText}
