92b495b093be8562ba7b8b7526e0af78f8dbbb6e
rabin245 <drarabin@gmail.com>
2023-03-11 16:57:43 +0545

    contract functions and events
    Co-authored-by: Aayuss Raut <aayussraut@users.noreply.github.com>

--- src/context/contractContext.jsx
+++ src/context/contractContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { ethers } from "ethers";
 import { abi, contractAddress } from "../constants/index";
+
 export const ContractContext = createContext();
 
 export const ContractContextProvider = ({ children }) => {
@@ -9,6 +10,12 @@ export const ContractContextProvider = ({ children }) => {
   const [contract, setContract] = useState(null);
   const [isMetaMaskInstalled, setIsMetaMaskInstalled] = useState(false);
 
+  const [isElectionStarted, setIsElectionStarted] = useState(false);
+  const [hasVoted, setHasVoted] = useState(false);
+  const [candidates, setCandidates] = useState([]);
+  const [currentVoterToCandidateId, setCurrentVoterToCandidateId] =
+    useState(null);
+
   async function init() {
     if (typeof window.ethereum !== "undefined") {
       setIsMetaMaskInstalled(true);
@@ -16,9 +23,9 @@ export const ContractContextProvider = ({ children }) => {
         method: "eth_requestAccounts",
       });
       console.log(addresses);
-      const provider = await new ethers.providers.Web3Provider(window.ethereum);
-      const signer = await provider.getSigner();
-      const contract = await new ethers.Contract(contractAddress, abi, signer);
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(contractAddress, abi, signer);
       setProvider(provider);
       setSigner(signer);
       setContract(contract);
@@ -26,11 +33,116 @@ export const ContractContextProvider = ({ children }) => {
       console.log("Please install MetaMask");
       setIsMetaMaskInstalled(false);
     }
+
+    // event listeners
+    contract.on("ElectionStarted", () => {
+      console.log("Election has started");
+      setIsElectionStarted(true);
+      getCandidates();
+    });
+
+    contract.on("ElectionEnded", () => {
+      console.log("Election has ended");
+      setIsElectionStarted(false);
+    });
+
+    contract.on("VoteCasted", (updatedCandidate) => {
+      console.log("Vote casted to candidate", updatedCandidate);
+      setCandidates((prev) => {
+        return prev.map((candidate) => {
+          if (candidate.id === updatedCandidate.id.toString()) {
+            return { ...candidate, votes: updatedCandidate.votes.toString() };
+          }
+          return candidate;
+        });
+      });
+    });
+
+    contract.on("CandidateAdded", (candidate) => {
+      console.log("Candidate added", candidate);
+      setCandidates((prev) => {
+        return [
+          ...prev,
+          {
+            id: candidate.id.toString(),
+            name: candidate.name,
+            partyName: candidate.partyName,
+            votes: candidate.votes.toString(),
+            imageUrl: candidate.imageUrl,
+          },
+        ];
+      });
+    });
+
+    contract.on("CandidateRemoved", (id) => {
+      console.log("Candidate removed", id);
+      setCandidates((prev) => {
+        return prev.filter((candidate) => candidate.id !== id.toString());
+      });
+    });
   }
+
+  // state changing functions
+  const startElection = async () => {
+    const transaction = await contract.startElection();
+    transaction.wait();
+  };
+  const endElection = async () => {
+    const transaction = await contract.endElection();
+    transaction.wait();
+  };
+
+  const voteToCandidate = async (id) => {
+    const transaction = await contract.voteToCandidate(id);
+    transaction.wait();
+  };
+
+  const addCandidate = async (id, name, partyName) => {
+    const transaction = await contract.addCandidate(id, name, partyName);
+    transaction.wait();
+  };
+
+  const removeCandidate = async (id) => {
+    const transaction = await contract.removeCandidate(id);
+    transaction.wait();
+  };
+
+  // view functions
+  const getHasVoted = async () => {
+    const hasVoted = await contract.hasVoted();
+    setHasVoted(hasVoted);
+  };
+
+  const getIsElectionStarted = async () => {
+    const isElectionStarted = await contract.getHasStarted();
+    setIsElectionStarted(isElectionStarted);
+  };
+
+  const getCurrentVoterToCandidateId = async () => {
+    const currentVoterToCandidateId =
+      await contract.currentVoterToCandidateId();
+    setCurrentVoterToCandidateId(currentVoterToCandidateId);
+  };
+
+  const getCandidates = async () => {
+    const candidates = await contract.getCandidates();
+    setCandidates(candidates);
+  };
 
   useEffect(() => {
     init();
+    getIsElectionStarted();
   }, []);
+
+  useEffect(() => {
+    // if (isElectionStarted) {
+    //   getHasVoted();
+    //   getCurrentVoterToCandidateId();
+    // }
+    getCandidates();
+    getHasVoted();
+    getCurrentVoterToCandidateId();
+  }, [isElectionStarted]);
 
   return (
     <ContractContext.Provider
@@ -39,6 +151,19 @@ export const ContractContextProvider = ({ children }) => {
         provider,
         signer,
         contract,
+        startElection,
+        endElection,
+        voteToCandidate,
+        addCandidate,
+        removeCandidate,
+        getCandidates,
+        getHasVoted,
+        getIsElectionStarted,
+        getCurrentVoterToCandidateId,
+        isElectionStarted,
+        hasVoted,
+        candidates,
+        currentVoterToCandidateId,
       }}
     >
       {children}
