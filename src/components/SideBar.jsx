097601b696598dd0714b2af44b6d1db36efeced6
rabin245 <drarabin@gmail.com>
2023-03-12 23:02:13 +0545

    add candidate page design

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -90,15 +90,15 @@ function SideBar() {
             <SideBarButton title="Candidates">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="icon icon-tabler icon-tabler-list-details"
-                width="24"
-                height="24"
+                className="icon icon-tabler icon-tabler-list-details"
+                width={24}
+                height={24}
                 viewBox="0 0 24 24"
-                stroke-width="1.75"
+                strokeWidth="1.75"
                 stroke="currentColor"
                 fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               >
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                 <path d="M13 5h8"></path>
@@ -111,6 +111,32 @@ function SideBar() {
             </SideBarButton>
           </Link>
         )}
+
+        {isAdmin && (
+          <Link to="/candidates/new">
+            <SideBarButton title="New Candidate">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon icon-tabler icon-tabler-user-plus"
+                width={24}
+                height={24}
+                viewBox="0 0 24 24"
+                strokeWidth="1.75"
+                stroke="currentColor"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0"></path>
+                <path d="M16 19h6"></path>
+                <path d="M19 16v6"></path>
+                <path d="M6 21v-2a4 4 0 0 1 4 -4h4"></path>
+              </svg>
+            </SideBarButton>
+          </Link>
+        )}
+
         <Link to="/results">
           <SideBarButton title="Results">
             <svg
--- src/pages/CreateCandidate.jsx
+++ src/pages/CreateCandidate.jsx
@@ -1,5 +1,63 @@
+import Banner from "../assets/banner.png";
+
 const CreateCandidate = () => {
-  return <div>create candidate</div>;
+  return (
+    <div className="mainDiv">
+      <div className="mainContent min-h-screen bg-white lg:ml-64">
+        <div className="">
+          <div className="banner">
+            <img src={Banner} alt="banner" />
+          </div>
+          <div className="px-4 py-2 max-w-screen-sm">
+            <div className="flex flex-col">
+              <h1 className="text-2xl font-bold text-gray-800">
+                Add Candidate
+              </h1>
+
+              <div className="mb-2">
+                <label className="text-gray-600 font-semibold block">
+                  Name
+                </label>
+                <input
+                  type="text"
+                  className="w-full px-4 py-2 mt-2 text-sm border rounded-md
+                         focus:border-blue-500 focus:outline-blue-500"
+                />
+              </div>
+
+              <div className="mb-2">
+                <label className="text-gray-600 font-semibold block">
+                  Party
+                </label>
+                <input
+                  type="text"
+                  className="w-full px-4 py-2 mt-2 text-sm border rounded-md
+                         focus:border-blue-500 focus:outline-blue-500"
+                />
+              </div>
+
+              <div className="mb-2">
+                <label className="text-gray-600 font-semibold block">
+                  Image URL
+                </label>
+                <input
+                  type="text"
+                  className="w-full px-4 py-2 mt-2 text-sm border rounded-md
+                         focus:border-blue-500 focus:outline-blue-500"
+                />
+              </div>
+
+              <div className="mt-2">
+                <button className="bg-blue-500 p-3 w-44 text-md font-bold text-white rounded-lg hover:shadow-2xl hover:bg-blue-600 active:ring-1 active:ring-gray-400">
+                  Create
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CreateCandidate;
