ba4b8ae7cc5d606329899eacdefb27d2ead2f5f5
rabin245 <drarabin@gmail.com>
2023-03-14 15:04:37 +0545

    add bar chart in results page

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,16 +1,50 @@
-import { useContext, useMemo } from "react";
+import { useContext, useEffect, useMemo, useState } from "react";
 import { ContractContext } from "../context/contractContext";
+import { CategoryScale, Chart } from "chart.js/auto";
+import { BarChart } from "../components/BarChart";
+
+Chart.register(CategoryScale);
 
 function Results() {
   const { candidates } = useContext(ContractContext);
 
   const resultsList = useMemo(() => {
     return [...candidates].sort((a, b) => {
-      const aVotes = parseInt(a.votes.toString());
-      const bVotes = parseInt(b.votes.toString());
+      const aVotes = parseInt(a.votes);
+      const bVotes = parseInt(b.votes);
       return bVotes - aVotes;
     });
   }, [candidates]);
+
+  const chartData = useMemo(() => {
+    return {
+      labels: candidates.map((candidate) => candidate.name),
+      datasets: [
+        {
+          label: "Votes",
+          data: candidates.map((candidate) => candidate.votes),
+          backgroundColor: ["rgb(96 165 250)", "rgb(56 189 248)"],
+          borderWidth: 1,
+        },
+      ],
+    };
+  }, [candidates]);
+
+  const options = {
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+    maintainAspectRatio: false,
+    scales: {
+      x: {
+        grid: {
+          display: false,
+        },
+      },
+    },
+  };
 
   return (
     <div className="mainDiv">
@@ -26,7 +60,7 @@ function Results() {
                 <th>Acquired Votes</th>
               </tr>
             </thead>
-            <tbody className="border border-gray-300  ">
+            <tbody className="border border-gray-300">
               {resultsList.map((row, index) => (
                 <tr key={row.id.toString()} className="even:bg-blue-100">
                   <td>{index}</td>
@@ -38,6 +72,7 @@ function Results() {
             </tbody>
           </table>
         </div>
+        <BarChart chartData={chartData} options={options} />
       </div>
     </div>
   );
